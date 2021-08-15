a8cf5295f4147531109b751cd3dd310a4a04ab11
Ibtiseam <ibtissem.bouzidii@gmail.com>
2021-08-15 18:31:48 +0200

    update webConfig

--- src/main/java/com/example/AZplastering/security/WebSecurityConfig.java
+++ src/main/java/com/example/AZplastering/security/WebSecurityConfig.java
@@ -63,6 +63,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/test/**").permitAll()
                 .anyRequest().authenticated();
 
-        http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
